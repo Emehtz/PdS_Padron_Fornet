74bdf56786c329fbadf68e8353e9fdf4b435bfe8
Emehtz <emehtz@gmail.com>
2023-04-25 17:38:11 +0200

    Corregidos los errores del commit anterior y ahora imprime el valore absoluto de la FFT de cada una de las muestras de forma secuencial

--- TEC/src/Tarea1.py
+++ TEC/src/Tarea1.py
@@ -45,49 +45,51 @@ def FastFT(buffer):
 # Creamos Wk*n con N/2 (matriz 5x5 con exp(-j*2*pi*k*n/5) variando k en las filas y n en las columnas o viceversa) k y n varia de
 # 0  a 4 (0-((N/2)-1)).
     Wkn = [[0 for n in range(5)] for k in range(5)] # Matriz de ceros (5x5)
-for n in range (5):
-    for k in range (5):
-        Wkn[n][k]=cmath.exp(-1j*2*cmath.pi*k*n/5)
+    for n in range (5):
+        for k in range (5):
+            Wkn[n][k]=cmath.exp(-1j*2*cmath.pi*k*n/5)
 # Necesitamos una matriz Wk con N (matriz 1x5 ó 5x1 con exp(-j*2*pi*k/10) variando k de 0-4, para ello podemos usar la matriz anterios
 # fijando n a 1, y sumando a cada elemento de esa fila o columna por exp(1/2). 
-Wk = [0 for n in range(5)]  # Matriz de ceros (5x1)
-for k in range (5):
-    Wk[k]=Wkn[1][k]*cmath.exp(0.5)
+    Wk = [0 for n in range(5)]  # Matriz de ceros (5x1)
+    for k in range (5):
+        Wk[k]=Wkn[1][k]*cmath.exp(0.5)
 # Generamos las funciones f(n)=x(2*n) y g(n)=x(2*n+1).
-buffer=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-fn=[0 for n in range(5)]
-gn=[0 for n in range(5)]
-for i in range (10):
-    if i % 2 == 0:
-        n=int(i/2)
-        fn[n]=buffer[i]         
-    else:
-        n=int((i-1)/2)
-        gn[n]=buffer[i]         
+    
+    fn=[0 for n in range(5)]
+    gn=[0 for n in range(5)]
+    for i in range (10):
+        if i % 2 == 0:
+            n=int(i/2)
+            fn[n]=buffer[i]         
+        else:
+            n=int((i-1)/2)
+            gn[n]=buffer[i]
 
 # Calculamos la DFT de de f(n) (F(k)) usando Wk*n con N/2 haciendo el sumatorio desde 0 hasta 4 de f(n)*Wk*n, siendo K
 #k la variable independiente de la DFT.
 
-Fk=[0 for n in range(5)]
-for k in range (5):
-    for n in range (5):
-        Fk[k]=fn[k]*Wkn[n][k]+Fk[k]
+    Fk=[0 for n in range(5)]
+    for k in range (5):
+        for n in range (5):
+            Fk[k]=fn[k]*Wkn[n][k]+Fk[k]
 
 # Repetimos el proceso anterior para calcular G(k) a partir de g(n).
 
-Gk=[0 for n in range(5)]
-for k in range (5):
-    for n in range (5):
-        Gk[k]=gn[k]*Wkn[n]+Gk[k]
+    Gk=[0 for n in range(5)]
+    for k in range (5):
+        for n in range (5):
+            Gk[k]=gn[k]*Wkn[n][k]+Gk[k]
 
 # Obtenmos la DFT X[k] a partir de de las F(k) y G(k) sabiendo que para k menor a 5 la formula es
 #X(k)=F(k) + Wk*G(k) y para k mayor a 5 X(k)=F(k-5)-W(k-5)*G(k-5).
 
-Xk=[0 for n in range(10)]
-for k in range (5):
-    Xk[k]=Fk[k]+Wk[k]*Gk[k]
-for k in range (5):
-    Xk[k+5]=Fk[k]-Wk[k]*Gk[k]
+    Xk=[0 for n in range(10)]
+    for k in range (5):
+        Xk[k]=Fk[k]+Wk[k]*Gk[k]
+    for k in range (5):
+        Xk[k+5]=Fk[k]-Wk[k]*Gk[k]
+        
+    return(Xk) 
 # ------------------------------------------------------------------------------
 # Bucle principal
 # ------------------------------------------------------------------------------
@@ -118,6 +120,10 @@ def loop():
               y = readInput()
               led.duty_u16(u);
               buffer[i]=y
+              Yw = FastFT(buffer)
+              for j in range (10):
+                  yywn = abs(Yw[j])
+                  print(f'{u} {y} {yywn}')
               writeOutput(int(u))
             except ValueError:
               pass
@@ -126,7 +132,7 @@ def loop():
         if spoll.poll(0):
             cmd = str(sys.stdin.readline())
             parse_command(cmd)
-        print(f'{u} {y}')
+       
         
         
 # ------------------------------------------------------------------------------
@@ -139,7 +145,7 @@ BUFFER_SIZE = 10 # Muestras en el buffer
 
 def signal(t):
   # Pon aquí el código necesario para generar tu señal.
-  freq = 0.05
+  freq = 0.01
   signal_out = math.sin( 2*math.pi*freq * t) * 65_025
   return int(math.fabs(signal_out))
 
